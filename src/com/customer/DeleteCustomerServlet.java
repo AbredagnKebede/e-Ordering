f94d23da7a52976ed02327ea87b1d4ad5bf7c633
Henok-Demissie <101617532+Henok-Demissie@users.noreply.github.com>
2025-01-02 10:26:23 +0400

    Update DeleteCustomerServlet.java

--- src/com/customer/DeleteCustomerServlet.java
+++ src/com/customer/DeleteCustomerServlet.java
@@ -10,33 +10,46 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+/**
+ * Servlet implementation for deleting a customer from the database.
+ */
 @WebServlet("/DeleteCustomerServlet")
 public class DeleteCustomerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Handles POST requests for deleting a customer.
+	 * 
+	 * @param request  The HttpServletRequest object that contains the request
+	 *                 from the client.
+	 * @param response The HttpServletResponse object that contains the response
+	 *                 to send to the client.
+	 * @throws ServletException If an error occurs during request handling.
+	 * @throws IOException      If an input or output error occurs during request
+	 *                         handling.
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		// Retrieve the customer ID from the request
 		String id = request.getParameter("cusid");
-		boolean isTrue;
+		boolean isDeleted;
+
+		// Call the method to delete the customer
+		isDeleted = CustomerDBUtil.deleteCustomer(id);
 		
-		isTrue = CustomerDBUtil.deleteCustomer(id);
-		
-		if (isTrue == true) {
+		// Check if the deletion was successful
+		if (isDeleted) {
+			// If successful, redirect to the customer insert page
 			RequestDispatcher dispatcher = request.getRequestDispatcher("customerinsert.jsp");
 			dispatcher.forward(request, response);
-		}
-		else {
-			
+		} else {
+			// If deletion failed, retrieve customer details for display
 			List<Customer> cusDetails = CustomerDBUtil.getCustomerDetails(id);
-			request.setAttribute("cusDetails", cusDetails);
+			request.setAttribute("cusDetails", cusDetails); // Set customer details as a request attribute
 			
+			// Forward the request to the user account page for feedback
 			RequestDispatcher dispatcher = request.getRequestDispatcher("useraccount.jsp");
 			dispatcher.forward(request, response);
 		}
-		
-		
-		
 	}
-
 }

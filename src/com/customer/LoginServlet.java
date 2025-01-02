c86e3593830347074c506cfab2144a5cec127bdd
Henok-Demissie <101617532+Henok-Demissie@users.noreply.github.com>
2025-01-02 10:27:30 +0400

    Update LoginServlet.java

--- src/com/customer/LoginServlet.java
+++ src/com/customer/LoginServlet.java
@@ -11,35 +11,50 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+/**
+ * Servlet implementation for handling user login.
+ */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Handles POST requests for user login.
+	 * 
+	 * @param request  The HttpServletRequest object that contains the request
+	 *                 from the client.
+	 * @param response The HttpServletResponse object that contains the response
+	 *                 to send to the client.
+	 * @throws ServletException If an error occurs during request handling.
+	 * @throws IOException      If an input or output error occurs during request
+	 *                         handling.
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		PrintWriter out = response.getWriter();
 		response.setContentType("text/html");
-		
+		PrintWriter out = response.getWriter(); // Initialize PrintWriter for output
+
+		// Retrieve username and password from the request
 		String userName = request.getParameter("username");
 		String password = request.getParameter("password");
-		boolean isTrue;
+
+		// Validate user credentials
+		boolean isValidUser = CustomerDBUtil.validate(userName, password);
 		
-		isTrue = CustomerDBUtil.validate(userName, password);
-		
-		if (isTrue == true) {
+		if (isValidUser) {
+			// If validation is successful, retrieve customer details
 			List<Customer> cusDetails = CustomerDBUtil.getCustomer(userName);
 			request.setAttribute("cusDetails", cusDetails);
-			
-			RequestDispatcher dis = request.getRequestDispatcher("useraccount.jsp");
-			dis.forward(request, response);
+
+			// Forward to user account page
+			RequestDispatcher dispatcher = request.getRequestDispatcher("useraccount.jsp");
+			dispatcher.forward(request, response);
 		} else {
+			// If validation fails, send an alert and redirect to login page
 			out.println("<script type='text/javascript'>");
-			out.println("alert('Your username or password is incorrect');");
-			out.println("location='profilelogin.jsp'");
+			out.println("alert('Your username or password is incorrect. Please try again.');");
+			out.println("location='profilelogin.jsp';");
 			out.println("</script>");
 		}
-		
 	}
-
 }

c7b81e9ad08bfe6618bb8fe982c96c45360fcba8
Henok-Demissie <101617532+Henok-Demissie@users.noreply.github.com>
2025-01-02 10:28:21 +0400

    Update UpdateCustomerServlet.java

--- src/com/customer/UpdateCustomerServlet.java
+++ src/com/customer/UpdateCustomerServlet.java
@@ -10,12 +10,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet implementation for updating customer information.
+ */
 @WebServlet("/UpdateCustomerServlet")
 public class UpdateCustomerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Handles POST requests for updating a customer's information.
+	 * 
+	 * @param request  The HttpServletRequest object that contains the request
+	 *                 from the client.
+	 * @param response The HttpServletResponse object that contains the response
+	 *                 to send to the client.
+	 * @throws ServletException If an error occurs during request handling.
+	 * @throws IOException      If an input or output error occurs during request
+	 *                         handling.
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		// Retrieve customer details from the request parameters
 		String id = request.getParameter("cusid");
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
@@ -24,26 +39,16 @@ public class UpdateCustomerServlet extends HttpServlet {
 		String username = request.getParameter("uname");
 		String password = request.getParameter("pass");
 		String role = request.getParameter("role");
-		boolean isTrue;
 		
-		isTrue = CustomerDBUtil.updatecustomer(id, name, email, phone, address,username, password, role);
+		// Variable to track the success of the update operation
+		boolean isUpdated = CustomerDBUtil.updatecustomer(id, name, email, phone, address, username, password, role);
 		
-		if(isTrue == true) {
-			
-			List<Customer> cusDetails = CustomerDBUtil.getCustomerDetails(id);
-			request.setAttribute("cusDetails", cusDetails);
-			
-			RequestDispatcher dis = request.getRequestDispatcher("useraccount.jsp");
-			dis.forward(request, response);
-		}
-		else {
-			
-			List<Customer> cusDetails = CustomerDBUtil.getCustomerDetails(id);
-			request.setAttribute("cusDetails", cusDetails);
-			
-			RequestDispatcher dis = request.getRequestDispatcher("useraccount.jsp");
-			dis.forward(request, response);
-		}
-	}
+		// Retrieve customer details to display on user account page
+		List<Customer> cusDetails = CustomerDBUtil.getCustomerDetails(id);
+		request.setAttribute("cusDetails", cusDetails); // Set customer details as a request attribute
 
+		// Forward to the user account page regardless of the update success
+		RequestDispatcher dispatcher = request.getRequestDispatcher("useraccount.jsp");
+		dispatcher.forward(request, response);
+	}
 }

d434ee1e784ec817742591ce140eeb3ebc3cf348
Henok-Demissie <101617532+Henok-Demissie@users.noreply.github.com>
2025-01-07 00:53:37 +0400

    Update ProductController.java

--- src/com/ManageMenu/ProductController.java
+++ src/com/ManageMenu/ProductController.java
@@ -11,10 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * ProductController.java
- * Servlet for handling product management operations, such as adding, editing, viewing, and deleting products.
- */
+
 @WebServlet("/ProductController")
 public class ProductController extends HttpServlet {
     private static final long serialVersionUID = 1L;
